714eef7ede0685664ad69c78f16fcc53aed8fd42
Sybran <Sybran@LAPTOP-QHODVUE8>
2021-01-13 17:31:53 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    message

--- Assets/Scripts/RabbitBehavior.cs
+++ Assets/Scripts/RabbitBehavior.cs
@@ -9,30 +9,99 @@ public class RabbitBehavior : MonoBehaviour
     [SerializeField] float maxSpeed;
     [SerializeField] float range;
     [SerializeField] float maxDistance;
+    [SerializeField] public float hearingRadius;
+    float currentSpeed;
+    public Vector3 waypoint;
+    [SerializeField] bool isFleeing = false;
 
-    Vector3 waypoint;
+    public LayerMask targetMask;
     // Start is called before the first frame update
     void Start()
     {
+        currentSpeed = speed;
         SetNewDest();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, waypoint, speed * Time.deltaTime);
-        Vector3 vectorToTarget = waypoint - transform.position;
-        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        const float angleSpeed = 10f;
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * angleSpeed);
-
-        if (Vector2.Distance(transform.position, waypoint) < range)
-            SetNewDest();
+        TargetsInFOV();
+        TargetsInHearingRange();
+        if (isFleeing)
+        {
+            UpdateRabbitFlee();
+        }
+        else
+        {
+            UpdateWander();
+        }
     }
 
     void SetNewDest()
     {
-        waypoint = new Vector2(Random.Range(-maxDistance, maxDistance), Random.Range(-maxDistance, maxDistance));
+        waypoint = new Vector3(Random.Range(-maxDistance, maxDistance), Random.Range(-maxDistance, maxDistance));
+    }
+
+    void UpdateWander()
+    {
+        currentSpeed = speed;
+        transform.position = Vector2.MoveTowards(transform.position, waypoint, currentSpeed * Time.deltaTime);
+        RotateTowards(waypoint);
+
+        if (Vector2.Distance(transform.position, waypoint) < range)
+            SetNewDest();
+    }
+    void UpdateRabbitFlee()
+    {
+        currentSpeed = maxSpeed;
+
+        //transform.position = Vector2.MoveTowards(transform.position, waypoint, -1 * currentSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, waypoint, -1 * currentSpeed * Time.deltaTime);
+        Vector3 vectorFromTarget = transform.position - waypoint;
+        RotateTowards(vectorFromTarget);
+    }
+
+    void TargetsInFOV()
+    {
+        FOV[] eyes = GetComponentsInChildren<FOV>();
+        List<Transform> targets = new List<Transform>();//GetComponentInChildren<FOV>().visibleTargets;
+        for (int i = 0; i < eyes.Length; i++)
+        {
+            List<Transform> t = eyes[i].visibleTargets;
+            for (int j = 0; j < t.Count; j++)
+            {
+                targets.Add(t[j]);
+            }
+        }
+
+        int targetCount = targets.Count;
+        if (targetCount > 0)
+        {
+            for (int i = 0; i < targetCount; i++)
+            {
+                waypoint += targets[i].position;
+            }
+            waypoint /= targetCount;
+            isFleeing = true;
+        }
+    }
+
+    void TargetsInHearingRange()
+    {
+        Collider2D[] targetsInHearingRadius = Physics2D.OverlapCircleAll(transform.position, hearingRadius, targetMask);
+        if (isFleeing && targetsInHearingRadius.Length == 0)
+        {
+            isFleeing = false;
+            SetNewDest();
+        }
+    }
+
+    void RotateTowards(Vector3 target)
+    {
+        Vector3 vectorToTarget = target - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        const float angleSpeed = 10f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * angleSpeed);
     }
 }
